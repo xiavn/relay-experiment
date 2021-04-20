0b711d7277d801d73d14d0b5d71af2455a4876f5
lturnbullcdw <l.turnbull@uk.cdw.com>
2021-04-20 17:00:34 +0100

    Set up basic fetch of repo name

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import fetchGraphQL from './fetch-graphql';
 
 function App() {
+    const [name, setName] = useState<string | null>(null);
+
+    useEffect(() => {
+        let isMounted = true;
+        fetchGraphQL(`
+        query RepositoryNameQuery {
+            # feel free to change owner/name here
+            repository(owner: "facebook" name: "relay") {
+              name
+            }
+          }
+        `)
+            .then((response) => {
+                if (!isMounted) {
+                    return;
+                }
+                const data = response.data;
+                setName(data.repository.name);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        return () => {
+            isMounted = false;
+        };
+    }, [fetchGraphQL, setName]);
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>{name != null ? `Repository: ${name}` : 'Loading'}</p>
             </header>
         </div>
     );
