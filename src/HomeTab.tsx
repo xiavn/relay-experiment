01ae92add6cbb215fdfc60d6a355da055fd4104e
lturnbullcdw <l.turnbull@uk.cdw.com>
2021-06-24 17:13:39 +0100

    Add in lots of extra fields to links

--- src/HomeTab.tsx
+++ src/HomeTab.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { PreloadedQuery, usePreloadedQuery } from 'react-relay';
 import graphql from 'babel-plugin-relay/macro';
 import { HomeTabFeedQuery } from '__generated__/HomeTabFeedQuery.graphql';
@@ -9,19 +9,71 @@ const FeedQuery = graphql`
             description
             url
             id
+            postedBy {
+                email
+                name
+                faveColour {
+                    hexValue
+                }
+            }
+            votes(first: 5) {
+                pageCursors {
+                    totalRecords
+                }
+            }
         }
     }
 `;
 
 function HomeTab(props: { queryRef: PreloadedQuery<HomeTabFeedQuery> }) {
     const data = usePreloadedQuery<HomeTabFeedQuery>(FeedQuery, props.queryRef);
-
     return (
-        <div>
+        <div
+            style={{
+                padding: '4px',
+            }}
+        >
+            <h2>Feed</h2>
             {data.feed.map((link) => (
-                <div key={link?.id}>
-                    <p>{link?.url}</p>
-                    <p>{link?.description}</p>
+                <div
+                    key={link?.id}
+                    style={{
+                        border: '2px darkcyan solid',
+                        background: 'azure',
+                        marginBottom: '4px',
+                        padding: '4px',
+                    }}
+                >
+                    <dl>
+                        <dt>
+                            <strong>Url:</strong>
+                        </dt>
+                        <dd>{link?.url}</dd>
+                        <dt>
+                            <strong>Description:</strong>
+                        </dt>
+                        <dd>{link?.description}</dd>
+                        {link?.postedBy !== null && (
+                            <Fragment>
+                                <dt>
+                                    <strong>Posted By:</strong>
+                                </dt>
+                                <dd
+                                    style={{
+                                        color:
+                                            link?.postedBy.faveColour.hexValue,
+                                    }}
+                                >
+                                    {link?.postedBy.name} -{' '}
+                                    {link?.postedBy.email}
+                                </dd>
+                            </Fragment>
+                        )}
+                        <dt>
+                            <strong>Votes:</strong>
+                        </dt>
+                        <dd>{link?.votes?.pageCursors?.totalRecords || 0}</dd>
+                    </dl>
                 </div>
             ))}
         </div>
--- src/__generated__/HomeTabFeedQuery.graphql.ts
+++ src/__generated__/HomeTabFeedQuery.graphql.ts
@@ -9,6 +9,18 @@ export type HomeTabFeedQueryResponse = {
         readonly description: string | null;
         readonly url: string | null;
         readonly id: string;
+        readonly postedBy: {
+            readonly email: string;
+            readonly name: string;
+            readonly faveColour: {
+                readonly hexValue: string;
+            };
+        } | null;
+        readonly votes: {
+            readonly pageCursors: {
+                readonly totalRecords: number;
+            } | null;
+        } | null;
     } | null>;
 };
 export type HomeTabFeedQuery = {
@@ -24,52 +36,150 @@ query HomeTabFeedQuery {
     description
     url
     id
+    postedBy {
+      email
+      name
+      faveColour {
+        hexValue
+        id
+      }
+      id
+    }
+    votes(first: 5) {
+      pageCursors {
+        totalRecords
+      }
+    }
   }
 }
 */
 
 const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "alias": null,
-    "args": null,
-    "concreteType": "Link",
-    "kind": "LinkedField",
-    "name": "feed",
-    "plural": true,
-    "selections": [
-      {
-        "alias": null,
-        "args": null,
-        "kind": "ScalarField",
-        "name": "description",
-        "storageKey": null
-      },
-      {
-        "alias": null,
-        "args": null,
-        "kind": "ScalarField",
-        "name": "url",
-        "storageKey": null
-      },
-      {
-        "alias": null,
-        "args": null,
-        "kind": "ScalarField",
-        "name": "id",
-        "storageKey": null
-      }
-    ],
-    "storageKey": null
-  }
-];
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "description",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "url",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "email",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "hexValue",
+  "storageKey": null
+},
+v6 = {
+  "alias": null,
+  "args": [
+    {
+      "kind": "Literal",
+      "name": "first",
+      "value": 5
+    }
+  ],
+  "concreteType": "LinkVotes_Connection",
+  "kind": "LinkedField",
+  "name": "votes",
+  "plural": false,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "PageCursors",
+      "kind": "LinkedField",
+      "name": "pageCursors",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "totalRecords",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "storageKey": "votes(first:5)"
+};
 return {
   "fragment": {
     "argumentDefinitions": [],
     "kind": "Fragment",
     "metadata": null,
     "name": "HomeTabFeedQuery",
-    "selections": (v0/*: any*/),
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Link",
+        "kind": "LinkedField",
+        "name": "feed",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/),
+          (v1/*: any*/),
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "User",
+            "kind": "LinkedField",
+            "name": "postedBy",
+            "plural": false,
+            "selections": [
+              (v3/*: any*/),
+              (v4/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Colour",
+                "kind": "LinkedField",
+                "name": "faveColour",
+                "plural": false,
+                "selections": [
+                  (v5/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          (v6/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
     "type": "Query",
     "abstractKey": null
   },
@@ -78,17 +188,60 @@ return {
     "argumentDefinitions": [],
     "kind": "Operation",
     "name": "HomeTabFeedQuery",
-    "selections": (v0/*: any*/)
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Link",
+        "kind": "LinkedField",
+        "name": "feed",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/),
+          (v1/*: any*/),
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "User",
+            "kind": "LinkedField",
+            "name": "postedBy",
+            "plural": false,
+            "selections": [
+              (v3/*: any*/),
+              (v4/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Colour",
+                "kind": "LinkedField",
+                "name": "faveColour",
+                "plural": false,
+                "selections": [
+                  (v5/*: any*/),
+                  (v2/*: any*/)
+                ],
+                "storageKey": null
+              },
+              (v2/*: any*/)
+            ],
+            "storageKey": null
+          },
+          (v6/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ]
   },
   "params": {
-    "cacheID": "97d584d67081da66c72990ce97d08826",
+    "cacheID": "54aaebfb5cc0d77620215911406d45d6",
     "id": null,
     "metadata": {},
     "name": "HomeTabFeedQuery",
     "operationKind": "query",
-    "text": "query HomeTabFeedQuery {\n  feed {\n    description\n    url\n    id\n  }\n}\n"
+    "text": "query HomeTabFeedQuery {\n  feed {\n    description\n    url\n    id\n    postedBy {\n      email\n      name\n      faveColour {\n        hexValue\n        id\n      }\n      id\n    }\n    votes(first: 5) {\n      pageCursors {\n        totalRecords\n      }\n    }\n  }\n}\n"
   }
 };
 })();
-(node as any).hash = 'c6fa237b28d426687d8a2e0b6571b084';
+(node as any).hash = '5ee7fa3e62e84c6d787e1c39ad8cfcde';
 export default node;
